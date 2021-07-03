a488bb2e4000a37de828c78f89480b8be4bcb232
Javier Wasserman <jamarks@gmail.com>
2021-07-03 19:24:17 +0200

    json_

--- pages/api/data.js
+++ pages/api/data.js
@@ -2,45 +2,45 @@
 
 
 const users = [{
-	"name": "Claudia Lewin",
-	"permalink": "abg-claudia-lewin",
-	"photo": "https://media-exp3.licdn.com/dms/image/C4D03AQErYA_Z8MqvAA/profile-displayphoto-shrink_800_800/0/1621239308415?e=1630540800&v=beta&t=gq5F5IFxDUF9iQP1W3P_ZnU9eHbXguAczpRFs128C2c",
-	"linkedin": "https://www.linkedin.com",
-  "companyname": "ABG REAL ESTATE GROUP",
-	"phone": "+491774871153",
-  "location": "Berlin, Deutschland",
-  "email": "claudia.lewin@abg-group.de",
-  "address": "Kurfuntendamm 22, 10719, Berlin, Germany",
-  "titlesmall": "MIRCS",
-  "title": "Senior Projektmanagerin",
-  "aboutme": "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-	"custom": [{
-		"name": "Personal Website",
-		"url": "http://www.google.com"
+	name: "Claudia Lewin",
+	permalink: "abg-claudia-lewin",
+	phot: "https://media-exp3.licdn.com/dms/image/C4D03AQErYA_Z8MqvAA/profile-displayphoto-shrink_800_800/0/1621239308415?e=1630540800&v=beta&t=gq5F5IFxDUF9iQP1W3P_ZnU9eHbXguAczpRFs128C2c",
+	linkedin: "https://www.linkedin.com",
+  companyname: "ABG REAL ESTATE GROUP",
+	phone: "+491774871153",
+  location: "Berlin, Deutschland",
+  email: "claudia.lewin@abg-group.de",
+  address: "Kurfuntendamm 22, 10719, Berlin, Germany",
+  titlesmall: "MIRCS",
+  title: "Senior Projektmanagerin",
+  aboutme: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+	custom: [{
+		name: "Personal Website",
+		url: "https://www.google.com"
 	}, {
-		"name": "Corporate Website",
-		"url": "http://www.abg.com"
-	}]
-}, {
-	"name": "Javier Wasserman",
-	"permalink": "freelancer-Javier-Wasserman",
-	"photo": "https://via.placeholder.com/150",
-	"linkedin": "https://www.linkedin.com",
-  "companyname": "Freelancer",
-  "title": "Full Stack Dev Entrepreneur",
-  "aboutme": "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-  "location": "Berlin, Deutschland",
-	"phone": "+491774871153",
-	"custom": [{
-		"name": "Personal Website",
-		"url": "http://www.google.com"
+		name: "Corporate Website",
+		url: "http://www.abg.com"
+  }]
+},{
+	name: "Javier Wasserman",
+	permalink: "freelancer-javier-wasserman",
+	photo: "https://via.placeholder.com/150",
+	linkedin: "https://www.linkedin.com",
+  companyname: "Freelancer",
+  title: "Full Stack Dev Entrepreneur",
+  aboutme: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+  location: "Berlin, Deutschland",
+	phone: "+491774871153",
+	custom: [{
+	name: "Personal Website",
+		url: "http://www.google.com"
 	}, {
-		"name": "Corporate Website",
-		"url": "http://www.abg.com"
+		name: "Corporate Website",
+		url: "http://www.abg.com"
 	}]
 }]
 
  
 export default function data(req, res) {
-  res.status(200).json(users)
+  res.json(users)
 }
