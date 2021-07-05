1ae72fa75972569de18c2b2e87c23bc380fc83ce
Javier Wasserman <jamarks@gmail.com>
2021-07-05 19:05:08 +0200

    qr 2

--- pages/u/qr.js
+++ pages/u/qr.js
@@ -9,7 +9,7 @@ export default function Qrviewer({qrimage}){
  return(
  <>
  <div className='container w-full mx-auto py-10 px-10'>
-  <div className='w-full mx-auto mb-2 '>{name}</div>
+  <div className='w-full mx-auto mb-2 text-2xl'>{name}</div>
   <div>
    <img alt='qr' src={image} width='100%' height='100%'></img>
   </div>
