f36c8b594ec22d2b84769579289ca642823a4780
Javier Wasserman <jamarks@gmail.com>
2021-07-29 12:25:46 +0200

    secret

--- components/layout.js
+++ components/layout.js
@@ -16,7 +16,7 @@ const navigation = [
 ]
 
 
-const title = 'Bye, Business Card. Hello, QIQR. QR Generator', description = 'QR Generator and vCard Generator', keyworkds = 'vcard generator, qr generator', currentURL = 'https://qiqr.app', previewImage = '', siteName = 'QIQR app'
+const title = 'Bye, Business Card. Hello, QIQR. QR Generator', description = 'Digital Business Card + QR Generator and vCard Generator', keyworkds = 'digital business card, vcard generator, qr generator', currentURL = 'https://qiqr.app', previewImage = '', siteName = 'QIQR app'
 
 
 export default function Layout({ children }) {
--- pages/secret.js
+++ pages/secret.js
@@ -8,55 +8,70 @@ export default function Secret() {
 
 
 
- const [data, setData] = useState([{ id: 'test', name: 'test' }]);
+  const [data, setData] = useState([{ id: 'test', name: 'test' }]);
 
- /*useEffect(async () => {
-   const response = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/users`)
-   const data = await response.json()
+  /*useEffect(async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/users`)
+    const data = await response.json()
+  
+    setData(data.entriesData);
+    console.log(data.entriesData)
+  }, []);
  
-   setData(data.entriesData);
-   console.log(data.entriesData)
- }, []);
+  */
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_PROTOCOL + process.env.NEXT_PUBLIC_VERCEL_URL}/api/users`)
+      const data = await response.json()
 
- */
- useEffect(() => {
-  async function fetchData() {
-   const response = await fetch(`${process.env.NEXT_PUBLIC_PROTOCOL + process.env.NEXT_PUBLIC_VERCEL_URL}/api/users`)
-   const data = await response.json()
+      return (data.entriesData)
+    }
+    const users = fetchData()
+      .then(data => setData(data))
+  }, []); // Or [] if effect doesn't need props or state
 
-   return (data.entriesData)
-  }
-  const users = fetchData()
-   .then(data => setData(data))
- }, []); // Or [] if effect doesn't need props or state
+  return (
+    <div className='container'>
+      <Head>
+        <title>qrme</title>
+        <meta name="description" content="qrme" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
- return (
-  <div className='container'>
-   <Head>
-    <title>qrme</title>
-    <meta name="description" content="qrme" />
-    <link rel="icon" href="/favicon.ico" />
-   </Head>
+      <main className='container w-10/12 mx-auto'>
 
-   <main className='container w-10/12 mx-auto'>
+        <h1 className='my-4 text-md'>QIQR guests</h1>
+        <h2 className='size-md'>{data.length} records</h2>
+        <div className='flex flex-wrap '>
+          {data && data.map((item) => (
+            <div key={item.id} className='w-full md:w-4/12 px-2 py-2'>
+              <Link href={`${process.env.NEXT_PUBLIC_PROTOCOL}${process.env.NEXT_PUBLIC_VERCEL_URL}/u/${item.id}`}>
+                <a className=''>
+                  <div className=''>
+                    <div className="relative w-full h-48 md:w-1/2 rounded-t lg:rounded-t-none lg:rounded-r inline-block">
+                      {item.profilePhoto &&
+                        <Image unoptimized={true} className="w-full h-full absolute inset-0 object-cover rounded-t lg:rounded-r lg:rounded-t-none" layout="fill" src={item.profilePhoto} alt={item.profileName} />
+                      }
+                      {!item.profilePhoto &&
+                        <div className='w-full h-full bg-gray-400 opacity-75 mx-auto text-center pt-40'>[No photo yet]</div>
+                      }
+                    </div>
+                    <div className="w-full md:w-1/2 text-sm">
+                    {item.profileName}<br/>
+                    {item.profileCompanyName}<br/>
+                    {item.profileTitle}
+                    
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            </div>
 
-    <h1 className='my-4 text-md'>Some of other guests</h1>
-    <ul>
-     {data && data.map((item) => (
-      <li className='' key={item.id} >
-       <Link href={`${process.env.NEXT_PUBLIC_PROTOCOL}${process.env.NEXT_PUBLIC_VERCEL_URL}/u/${item.id}`}>
-        <a className=''>
-         {item.name} - {item.companyname}
-        </a>
-       </Link>
-      </li>
-     ))}
-    </ul>
-   </main>
+          ))}
 
-   <footer className='container w-10/12 mx-auto'>
-    by jamarks @ googlemail
-   </footer>
-  </div>
- )
+        </div>
+      </main>
+
+    </div>
+  )
 }
